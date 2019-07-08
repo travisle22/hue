4030bb6356f7a3ff1f83ea25d528683615fc9544
Travis Le <travis.le@dhcp-10-16-0-133.pa.cloudera.com>
2019-07-08 15:00:36 -0700

    Added/fixed test cases for filebrowser/views_test.py Fixed file encoding in compress_files_in_hdfs

--- apps/filebrowser/src/filebrowser/views_test.py
+++ apps/filebrowser/src/filebrowser/views_test.py
@@ -46,7 +46,6 @@ from desktop.conf import is_oozie_enabled
 from conf import MAX_SNAPPY_DECOMPRESSION_SIZE
 from lib.rwx import expand_mode
 from views import snappy_installed
-from nose import SkipTest
 from celery.utils import time
 
 
@@ -970,7 +969,7 @@ alert("XSS")
     prefix = self.cluster.fs_prefix + '/test_compress_files'
     self.cluster.fs.mkdir(prefix)
     
-    test_directories = ["test_dir1", "test dir2",  "test\tdir3", "testdir4" ]
+    test_directories = ["testdir", "test dir1", "test\tdir2",  "test\ndir3", "test\rdir4"]
     for temp_directories in test_directories:
         test_dir1 = prefix + "/" + temp_directories
         test_file = test_dir1 + '/test.txt'
@@ -981,16 +980,6 @@ alert("XSS")
             f = self.cluster.fs.open(test_file + "%s" %i, "w")
             f.close()
     
-    """
-    test_dir2 = prefix + '/test_dir2'
-    test_file = test_dir2 + '/test.txt'
-    self.cluster.fs.mkdir(test_dir2)
-    self.cluster.fs.chown(test_dir2, 'test')
-    self.cluster.fs.chmod(test_dir2, 0700)
-    for i in range(5) :
-        f = self.cluster.fs.open(test_file + "%s" %i, "w")
-        f.close()
-    """
     try:
       for temp_directories in test_directories :
           resp = self.c.post('/filebrowser/compress_files', {'upload_path': prefix, 'files[]': [temp_directories], 'archive_name': 'test_compress.zip'})
